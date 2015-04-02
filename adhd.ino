a34215953edb77bb9b4cb84e05bd20dfe6b1c432
Tim Kent <tim@kent.id.au>
2015-04-02 13:02:23 +1000

    Update adhd.ino
    Added logging for SSI-4 registers.
    Added code to light up 'L1' when logging successfully starts (will need to wire output 3 to L1 for this to work, or change the L1_LED_PIN definition to another output).
    Changed the way commas and newlines were being added so switch statement is easier to read and maintain.

--- adhd.ino
+++ adhd.ino
@@ -1,6 +1,6 @@
 /*
 
-Arduino Datalogging Hardware Device (ADHD) v0.6
+Arduino Datalogging Hardware Device (ADHD) v0.7
 For datalogging Adaptronic E420C and Select ECUs
 Written by Tim Kent <tim@kent.id.au>
 
@@ -11,7 +11,6 @@ Uses the following libraries:
 
 Connect Adaptronic ECU to real serial port, debug output is sent using SoftwareSerial.
 
-Tried to run this on a Uno but ran into memory problems, hopefully it is OK now I am using F() where possible.
 For an older Mega (pre R3), jumper 11, 12 and 13 on shield to 51, 50 and 52 on Mega.
 
 */
@@ -36,8 +35,13 @@ For an older Mega (pre R3), jumper 11, 12 and 13 on shield to 51, 50 and 52 on M
 // Modbus
 #include <SimpleModbusMaster.h>
 
-#ifdef ENABLE_DEBUG_SERIAL
+// L1 LED pin
+// Goes high when logging has successfully started
+// Comment out to disable
+#define L1_LED_PIN 3
+
 // Debugging output @ 19200 baud
+#ifdef ENABLE_DEBUG_SERIAL
 SoftwareSerial debugSerial(7, 8); // RX, TX
 #endif
 
@@ -57,12 +61,10 @@ File dataFile;
 #define RETRY_COUNT 0
 #define TX_ENABLE_PIN 2 // Not used for RS232 but needed for the library
 
-// The total amount of available memory on the master to store data
-#define TOTAL_NO_OF_REGISTERS 29
+// The total amount of available memory on the Modbus master to store data
+#define TOTAL_NO_OF_REGISTERS 33
 
-// This is the easiest way to create new packets
-// Add as many as you want. TOTAL_NO_OF_PACKETS
-// is automatically updated.
+// Modbus packet definitions
 enum
 {
   PACKET1,
@@ -71,22 +73,23 @@ enum
   PACKET4,
   PACKET5,
   PACKET6,
+  PACKET7,
   TOTAL_NO_OF_PACKETS // leave this last entry
 };
 
-// Create an array of Packets to be configured
+// Create an array of Modbus Packets to be configured
 Packet packets[TOTAL_NO_OF_PACKETS];
 
-// Master register array
+// Master Modbus register array
 unsigned int regs[TOTAL_NO_OF_REGISTERS];
 
-// Keeps track of last processed request
+// Keeps track of last processed Modbus request
 unsigned int lastSuccessfulRequest = 0;
 
 // Specify order of logging
-// regs are 4096-4116 (0-20), 4141 (21), 4152 (22), 4185 (23), 4196-4199 (24-27), 4203 (28)
+// regs are 4096-4116 (0-20), 4141 (21), 4152 (22), 4185 (23), 4196-4199 (24-27), 4203 (28), 4215-4218 (29-32)
 // Refer to e4xxx_ECU_ModbusRegDefn.xls for details
-const int logSequence[] = {0,1,7,2,3,4,5,6,8,10,11,9,20,12,16,22,23,21,24,25,26,27,28,13,14,15,17,18,19};
+const int logSequence[] = {0,1,7,2,3,4,5,6,8,10,11,9,20,12,16,22,23,21,24,25,26,27,28,13,14,15,17,18,19,29,30,31,32};
 
 void setup()
 {
@@ -106,7 +109,7 @@ void setup()
 #ifdef ENABLE_DEBUG_SERIAL
     debugSerial.println(F("RTC is NOT running!"));
 #endif
-    // following line sets the RTC to the date & time this sketch was compiled
+    // Following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
@@ -153,29 +156,42 @@ void setup()
   }
 
 #ifdef ENABLE_DEBUG_SERIAL
+#ifdef ENABLE_RTC
+  debugSerial.print(F("File opened: "));
+  debugSerial.println(dataFileName);
+#else
   debugSerial.println(F("File opened."));
+#endif
 #endif
 
   // Initialise each packet
-  // regs are 4096-4116, 4141, 4152, 4185, 4196-4199, 4203
   modbus_construct(&packets[PACKET1], 1, READ_HOLDING_REGISTERS, 4096, 21, 0);
   modbus_construct(&packets[PACKET2], 1, READ_HOLDING_REGISTERS, 4141, 1, 21);
   modbus_construct(&packets[PACKET3], 1, READ_HOLDING_REGISTERS, 4152, 1, 22);
   modbus_construct(&packets[PACKET4], 1, READ_HOLDING_REGISTERS, 4185, 1, 23);
   modbus_construct(&packets[PACKET5], 1, READ_HOLDING_REGISTERS, 4196, 4, 24);
   modbus_construct(&packets[PACKET6], 1, READ_HOLDING_REGISTERS, 4203, 1, 28);
+  // SSI-4 stuff
+  modbus_construct(&packets[PACKET7], 1, READ_HOLDING_REGISTERS, 4215, 4, 29);
 
-  // Initialize the Modbus finite state machine
+  // Initialise the Modbus finite state machine
   modbus_configure(&Serial, BAUD, SERIAL_8N1, TIMEOUT, POLLING, RETRY_COUNT, TX_ENABLE_PIN, packets, TOTAL_NO_OF_PACKETS, regs);
 
-  // Print the header line only once a Modbus request has succeeded
+  // Proceed only once a Modbus request has succeeded
   while (packets->successful_requests < 1) {
     modbus_update();
   }
-#ifdef ENABLE_DEBUG_SERIAL
-  debugSerial.println(F("Time (s),RPM,MAP (kPa),TPS (%),MAT (°C),WT (°C),AuxT (°C),Lambda,Knock,Idle,MVSS (km/h),SVSS (km/h),Batt (V),Trim (%),Inj 1 (ms),Ign 1 (°),Wastegate (%),Pump,Gear,ExtIn,EGT1 (°C),EGT2 (°C),EGT3 (°C),EGT4 (°C),Inj 2 (ms),Inj 3 (ms),Inj 4 (ms),Ign 2 (°),Ign 3 (°),Ign 4 (°)"));
+  
+#ifdef L1_LED_PIN
+  // We have a successful request, so enable L1 LED
+  digitalWrite(L1_LED_PIN, HIGH);
 #endif
-  dataFile.println(F("Time (s),RPM,MAP (kPa),TPS (%),MAT (°C),WT (°C),AuxT (°C),Lambda,Knock,Idle,MVSS (km/h),SVSS (km/h),Batt (V),Trim (%),Inj 1 (ms),Ign 1 (°),Wastegate (%),Pump,Gear,ExtIn,EGT1 (°C),EGT2 (°C),EGT3 (°C),EGT4 (°C),Inj 2 (ms),Inj 3 (ms),Inj 4 (ms),Ign 2 (°),Ign 3 (°),Ign 4 (°)"));
+  
+  // Print the header line
+#ifdef ENABLE_DEBUG_SERIAL
+  debugSerial.println(F("Time (s),RPM,MAP (kPa),TPS (%),MAT (°C),WT (°C),AuxT (°C),Lambda,Knock,Idle,MVSS (km/h),SVSS (km/h),Batt (V),Trim (%),Inj 1 (ms),Ign 1 (°),Wastegate (%),Pump,Gear,ExtIn,EGT1 (°C),EGT2 (°C),EGT3 (°C),EGT4 (°C),Inj 2 (ms),Inj 3 (ms),Inj 4 (ms),Ign 2 (°),Ign 3 (°),Ign 4 (°),Oil P,Fuel P,Aux P"));
+#endif
+  dataFile.println(F("Time (s),RPM,MAP (kPa),TPS (%),MAT (°C),WT (°C),AuxT (°C),Lambda,Knock,Idle,MVSS (km/h),SVSS (km/h),Batt (V),Trim (%),Inj 1 (ms),Ign 1 (°),Wastegate (%),Pump,Gear,ExtIn,EGT1 (°C),EGT2 (°C),EGT3 (°C),EGT4 (°C),Inj 2 (ms),Inj 3 (ms),Inj 4 (ms),Ign 2 (°),Ign 3 (°),Ign 4 (°),Oil P,Fuel P,Aux P"));
 }
 
 void loop()
@@ -204,30 +220,24 @@ void loop()
           // This will output in AFR instead of Lambda
           //debugSerial.print(regs[logSequence[i]]/2570.0, 1);
           debugSerial.print(regs[logSequence[i]]/2570.0/14.7, 2);
-          debugSerial.print(",");
 #endif
           // This will output in AFR instead of Lambda
           //dataFile.print(regs[logSequence[i]]/2570.0, 1);
           dataFile.print(regs[logSequence[i]]/2570.0/14.7, 2);
-          dataFile.print(",");
           break;
         // Knock needs to be divided by 256
         case 6:
 #ifdef ENABLE_DEBUG_SERIAL
           debugSerial.print(regs[logSequence[i]]/256);
-          debugSerial.print(",");
 #endif
           dataFile.print(regs[logSequence[i]]/256);
-          dataFile.print(",");
           break;
         // Battery needs to be divided by 10
         case 9:
 #ifdef ENABLE_DEBUG_SERIAL
           debugSerial.print(regs[logSequence[i]]/10.0, 1);
-          debugSerial.print(",");
 #endif
           dataFile.print(regs[logSequence[i]]/10.0, 1);
-          dataFile.print(",");
           break;
         // Injector needs to be divided by 1500
         case 12:
@@ -236,38 +246,38 @@ void loop()
         case 15:
 #ifdef ENABLE_DEBUG_SERIAL
           debugSerial.print(regs[logSequence[i]]/1500.0, 3);
-          debugSerial.print(",");
 #endif
           dataFile.print(regs[logSequence[i]]/1500.0, 3);
-          dataFile.print(",");
           break;
         // Timing needs to be divided by 5
         case 16:
         case 17:
         case 18:
-#ifdef ENABLE_DEBUG_SERIAL
-          debugSerial.print((int)regs[logSequence[i]]/5.0, 1);
-          debugSerial.print(",");
-#endif
-          dataFile.print((int)regs[logSequence[i]]/5.0, 1);
-          dataFile.print(",");
-          break;
-        // This is the last register in list of logSequence[]
-        // Same formatting as 16-18, but use println() and no trailing comma
         case 19:
 #ifdef ENABLE_DEBUG_SERIAL
-          debugSerial.println((int)regs[logSequence[i]]/5.0, 1);
+          debugSerial.print((int)regs[logSequence[i]]/5.0, 1);
 #endif
-          dataFile.println((int)regs[logSequence[i]]/5.0, 1);
+          dataFile.print((int)regs[logSequence[i]]/5.0, 1);
           break;
         // Everything else is untouched
         default:
 #ifdef ENABLE_DEBUG_SERIAL
           debugSerial.print((int)regs[logSequence[i]]);
-          debugSerial.print(",");
 #endif
           dataFile.print((int)regs[logSequence[i]]);
-          dataFile.print(",");
+      }
+      
+      // Add comma or newline
+      if (i == TOTAL_NO_OF_REGISTERS - 1) {
+#ifdef ENABLE_DEBUG_SERIAL
+        debugSerial.println();
+#endif
+        dataFile.println();
+      } else {
+#ifdef ENABLE_DEBUG_SERIAL
+        debugSerial.print(",");
+#endif
+        dataFile.print(",");
       }
     }
 
